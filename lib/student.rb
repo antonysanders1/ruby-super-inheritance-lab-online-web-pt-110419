469424008aa83504d0d8065fa9a140cdc7cbb4a3
Antony Sanders <antony.devon92@gmail.com>
2020-01-03 18:30:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,7 @@
 class Student 
-  
-  attr_accessor :student 
 
   def hello
-     return "Hey there! I'm so excited to learn stuff."  
+     "Hey there! I'm so excited to learn stuff."  
   end 
   
   def raise_hand
